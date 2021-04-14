521e4ea40538f4bb191c70e23449064612d74a41
Kacper Marjanski <kacpermarjanskipublic@gmail.com>
2021-04-14 20:30:31 +0200

    Init state

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/jsx-no-target-blank */
+import React from "react";
+import "./App.scss";
+
+const App = () => {
+  return <div className="App"></div>;
+};
+
+export default App;
